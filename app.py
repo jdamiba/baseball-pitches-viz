24e975f298e09a4b375a2d66f3320c4d7f29b182
Joseph Damiba <joseph.damiba@plot.ly>
2020-01-11 07:54:18 -0500

    setup

--- app.py
+++ app.py
@@ -1,461 +1,81 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
+import pandas as pd
+import pybaseball as pb
+import plotly.express as px
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-shapes = [
-  {
-    "type": "rect",
-    "layer": "below",
-    "x0": 0,
-    "x1": 0.5,
-    "y0": 0,
-    "y1": 0.5
-  },
-  {
-    "type": "rect",
-    "layer": "below",
-    "x0": 0.5,
-    "x1": 1,
-    "y0": 0,
-    "y1": 0.5
-  },
-  {
-    "type": "rect",
-    "layer": "below",
-    "x0": 0.5,
-    "x1": 1,
-    "y0": 0.5,
-    "y1": 1
-  },
-  {
-    "type": "rect",
-    "layer": "below",
-    "x0": 0,
-    "x1": 0.5,
-    "y0": 0.5,
-    "y1": 1
-  }
-]
-
-colors = [
-  "aquamarine",
-  "brown",
-  "chocolate",
-  "darkblue",
-  "darkgreen",
-  "forestgreen",
-  "gold",
-  "honeydew",
-  "indigo",
-  "navajowhite",
-  "khaki",
-  "lightblue",
-  "magenta",
-  "navy",
-  "orange",
-  "pink",
-  "aqua",
-  "red",
-  "silver",
-  "tomato",
-  "turquoise",
-  "violet",
-  "wheat",
-  "yellow",
-  "azure"
-]
-
-values = [256, 64, 16, 4, 1]
-
-parents = [255, 63, 15, 3, 0]
-
-uniformtext = {
-  "mode": ["false", "hide", "show"][1],
-  "minsize": 12
-}
-
-textposition = ["inside", "auto", "outside"][1]
-
-fig1 = {
-    "data": [
-    {
-      "parents": parents,
-      "labels": values,
-      "values": values,
-      "text": values,
-      "title": "insidetextorientation: horizontal",
-      "textposition": textposition,
-      "insidetextorientation": [
-        "auto",
-        "horizontal",
-        "radial",
-        "tangential"
-       ][1],
-      "type": "pie", 
-      "domain": {
-        "x": [
-          0.5,
-          1
-        ],
-        "y": [
-          0.5,
-          1
-        ]
-      }
-    },
-    {
-      "parents": parents,
-      "labels": values,
-      "values": values,
-      "text": values,
-      "title": "insidetextorientation: radial",
-      "textposition": textposition,
-      "insidetextorientation": [
-        "auto",
-        "horizontal",
-        "radial",
-        "tangential"
-       ][2],
-      "type": "pie", 
-      "domain": {
-        "x": [
-          0.5,
-          1
-        ],
-        "y": [
-          0,
-          0.5
-        ]
-      }
-    },
-    {
-      "parents": parents,
-      "labels": values,
-      "values": values,
-      "text": values,
-      "title": "insidetextorientation: tangential",
-      "textposition": textposition,
-      "insidetextorientation": [
-        "auto",
-        "horizontal",
-        "radial",
-        "tangential"
-       ][3],
-      "type": "pie", 
-      "domain": {
-        "x": [
-          0,
-          0.5
-        ],
-        "y": [
-          0,
-          0.5
-        ]
-      }
-    },
-    {
-      "parents": parents,
-      "labels": values,
-      "values": values,
-      "text": values,
-      "title": "insidetextorientation: auto",
-      "textposition": textposition,
-      "insidetextorientation": [
-        "auto",
-        "horizontal",
-        "radial",
-        "tangential"
-       ][0],
-      "type": "pie", 
-      "domain": {
-        "x": [
-          0,
-          0.5
-        ],
-        "y": [
-          0.5,
-          1
-        ]
-      }
-    }, 
-  ],
-  "layout": {
-    "uniformtext": uniformtext,
-    "margin": {
-      "t": 20,
-      "b": 10,
-      "l": 10,
-      "r": 10
-    },
-    "legend": {
-      "title": {
-        "text": "<b>legend title</b>"
-      }
-    },
-     "shapes": shapes,
-  },
-  
-}
-
-fig2_parents = [
-  "",
-  "Alpha",
-  "Alpha",
-  "Charlie",
-  "Charlie",
-  "Charlie",
-  "Foxtrot",
-  "Foxtrot",
-  "Foxtrot",
-  "Foxtrot",
-  "Juliet",
-  "Juliet",
-  "Juliet",
-  "Juliet",
-  "Juliet",
-  "Oscar",
-  "Oscar",
-  "Oscar",
-  "Oscar",
-  "Oscar",
-  "Oscar",
-  "Uniform",
-  "Uniform",
-  "Uniform",
-  "Uniform",
-  "Uniform",
-  "Uniform"
-]
-
-fig2_labels = [
-  "Alpha",
-  "Bravo",
-  "Charlie",
-  "Delta",
-  "Echo",
-  "Foxtrot",
-  "Golf",
-  "Hotel",
-  "India",
-  "Juliet",
-  "Kilo",
-  "Lima",
-  "Mike",
-  "November",
-  "Oscar",
-  "Papa",
-  "Quebec",
-  "Romeo",
-  "Sierra",
-  "Tango",
-  "Uniform",
-  "Victor",
-  "Whiskey",
-  "X ray",
-  "Yankee",
-  "Zulu"
-]
-
-fig2 = {
-    "data": [
-    {
-      "name": "horizontal",
-      "insidetextorientation": "horizontal",
-      "type": "sunburst",
-      "parents": fig2_parents,
-      "labels": fig2_labels,
-      "marker": {
-        "colors": colors
-      },
-      "textinfo": "label+value",
-      "domain": {
-        "x": [
-          0.5,
-          1
-        ],
-        "y": [
-          0.5,
-          1
-        ]
-      }
-    },
-    {
-      "name": "radial",
-      "insidetextorientation": "radial",
-      "type": "sunburst",
-      "parents": fig2_parents,
-      "labels": fig2_labels,
-      "textinfo": "label+value",
-      "domain": {
-        "x": [
-          0.5,
-          1
-        ],
-        "y": [
-          0,
-          0.5
-        ]
-      }
-    },
-    {
-      "name": "tangential",
-      "insidetextorientation": "tangential",
-      "type": "sunburst",
-      "parents": fig2_parents,
-      "labels": fig2_labels,
-      "textinfo": "label+value",
-      "domain": {
-        "x": [
-          0,
-          0.5
-        ],
-        "y": [
-          0,
-          0.5
-        ]
-      }
-    },
-    {
-      "title": "auto",
-      "insidetextorientation": "auto",
-      "type": "sunburst",
-      "parents": fig2_parents,
-      "labels": fig2_labels,
-      "textinfo": "label+value",
-      "domain": {
-        "x": [
-          0,
-          0.5
-        ],
-        "y": [
-          0.5,
-          1
-        ]
-      }
-    }
-  ],
-  "layout": {
-    "margin": {
-      "t": 10,
-      "b": 10,
-      "l": 10,
-      "r": 10
-    },
-    "showlegend": "True", 
-    "font": {
-      "size": 14
-    },
-    "shapes": [
-      {
-        "type": "rect",
-        "layer": "below",
-        "x0": 0,
-        "x1": 0.5,
-        "y0": 0,
-        "y1": 0.5
-      },
-      {
-        "type": "rect",
-        "layer": "below",
-        "x0": 0.5,
-        "x1": 1,
-        "y0": 0,
-        "y1": 0.5
-      },
-      {
-        "type": "rect",
-        "layer": "below",
-        "x0": 0.5,
-        "x1": 1,
-        "y0": 0.5,
-        "y1": 1
-      },
-      {
-        "type": "rect",
-        "layer": "below",
-        "x0": 0,
-        "x1": 0.5,
-        "y0": 0.5,
-        "y1": 1
-      }
-    ],
-    "annotations": [
-      {
-        "text": "auto",
-        "showarrow": "false",
-        "xref": "paper",
-        "yref": "paper",
-        "xanchor": "right",
-        "yanchor": "bottom",
-        "x": 0.5,
-        "y": 0.5,
-        "font": {
-          "size": 20
-        }
-      },
-      {
-        "text": "tangential",
-        "showarrow": "false",
-        "xref": "paper",
-        "yref": "paper",
-        "xanchor": "right",
-        "yanchor": "bottom",
-        "x": 0.5,
-        "y": 0,
-        "font": {
-          "size": 20
-        }
-      },
-      {
-        "text": "radial",
-        "showarrow": "false",
-        "xref": "paper",
-        "yref": "paper",
-        "xanchor": "right",
-        "yanchor": "bottom",
-        "x": 1,
-        "y": 0,
-        "font": {
-          "size": 20
-        }
-      },
-      {
-        "text": "horizontal",
-        "showarrow": "false",
-        "xref": "paper",
-        "yref": "paper",
-        "xanchor": "right",
-        "yanchor": "bottom",
-        "x": 1,
-        "y": 0.5,
-        "font": {
-          "size": 20
-        }
-      }
-    ]
-  }
-}
+external_stylesheets = ['https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
+  
+app.layout = html.Div(className="main", children=[
+  dcc.Markdown(children="## Visualizing A Season's Worth Of Pitches With [`pybaseball`](https://github.com/jldbc/pybaseball) and [`Dash`](https://dash.plot.ly)."),
+  html.Br(),
+  html.Br(),
+  dcc.Markdown(children="### Project Overview"),
+  dcc.Markdown(children="### Every day brings us closer to the beginning of the baseball season. One of the things I love about the game is the strong emphasis that is placed on collecting statistics about players. I could (and have) spend hours looking up interesting facts about the game. For example, did you know that [Sandy Koufax is the only pitcher to throw a perfect game while striking out a batter in every inning?](https://en.wikipedia.org/wiki/Sandy_Koufax%27s_perfect_game)"),
+  dcc.Markdown(children="### At the end of 2019 I discovered this [blog post](http://kenbo.hatenablog.com/entry/2019/12/18/001332) which demonstrates how to use the `pybaseball` API and Plotly Express to make really interesting vizualizations."),
+  dcc.Markdown(children="### I decided to build on that work by making a Dash app that would let you create a chart for any pitcher."),
+  dcc.Markdown(children="### Enter the name of a pitcher to see scatter and box plot visualizations of all the pitches they threw in the 2019 baseball season."),
+  html.Br(),
+  html.Br(),
+  dcc.Markdown(children="#### First Name"),
+  dcc.Input(id='first-name', value='Gerrit', type='text'),
+  html.Br(),
+  html.Br(),
+  dcc.Markdown(children="#### Last Name"),
+  dcc.Input(id='last-name', value='Cole', type='text'),
+  html.Br(),
+  html.Br(),
+  html.Button('Submit', id='button'),
+  dcc.Graph(id='scatter'),
+  dcc.Graph(id='box'),
+  dcc.Markdown(children="### built by [Joe Damiba](https://josephdamiba.com)"),
 
-app.layout = html.Div(children=[
-    dcc.Markdown(children='## This Dash app demonstrates the use of the `insidetextorientation` and `legend.title` attributes when creating charts.'),
-    dcc.Markdown(children='### [Codepen #1](https://codepen.io/MojtabaSamimi/pen/zYxBLJe)'),
-    dcc.Markdown(children='### [Codepen #2](https://codepen.io/MojtabaSamimi/pen/MWYpooK)'),
-    dcc.Markdown(children='### [GitHub PR](https://github.com/plotly/plotly.js/pull/4420)'),
-    dcc.Graph(
-        id='example-graph',
-        figure=fig1
-    ),
-    dcc.Graph(
-        id='example-graph-2',
-        figure=fig2
-    ),
 ])
 
+
+
+@app.callback(
+    [Output(component_id='scatter', component_property='figure'),
+     Output(component_id='box', component_property='figure')],
+    [Input('button', 'n_clicks')],
+    [State(component_id='first-name', component_property='value'),
+     State(component_id='last-name', component_property='value')])
+def update_output_div(n_clicks, first_name, last_name):
+  #only update on increment
+  prev_clicks = 0
+  if n_clicks is None or n_clicks == prev_clicks:
+    raise PreventUpdate
+  else: 
+    start_date = "2019-03-20" 
+    end_date = "2019-10-30"
+    
+    try:
+      key = pb.playerid_lookup(last_name, first_name)["key_mlbam"].values[0] # get unique pitcher identifier
+    except:
+      return px.scatter(title="No player found with that name. Check spelling?"), px.box(title="No player found with that name. Check spelling?"), 
+    
+    data = pb.statcast_pitcher(start_date, end_date, key) # get dataset of pitches thrown by pitcher
+    data = data.sort_values(["pitch_number"]) # sort pitches by order thrown, earliest first
+    data = data.dropna(subset=['pitch_type']) # make sure dataset does not contain nulls
+
+    data["seq"] = data.reset_index().index # create new column with pitch order
+        
+    scatter = px.scatter(data, x="seq", y="release_speed", color="pitch_type", 
+                            title=f'{first_name} {last_name} 2019 Pitches Release Speed Scatter Plot')
+    
+    box = px.box(data, x="pitch_type", y="release_speed", color="pitch_type", 
+                title=f'{first_name} {last_name} 2019 Pitches Release Speed Box Plot')
+    
+    prev_clicks = prev_clicks + 1
+    
+    return scatter, box
+  
 if __name__ == '__main__':
     app.run_server(debug=True)
     
