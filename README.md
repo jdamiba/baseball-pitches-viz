823cecf788820ff8b49e2016c0156af5f38581cb
Joseph Damiba <joseph.damiba@plot.ly>
2020-01-14 13:41:07 -0500

    updates

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
-# How to deploy a simple Plotly Dash app on Heroku
+We will query the pybaseball API to get data about pitches they threw from MLB.com's Statcast database.
 
-* You can view the finished app on [Heroku](https://flying-dog.herokuapp.com/).
-* Take a moment to read my [Medium post about deploying Dash apps](https://medium.com/@austinlasseter/how-to-deploy-a-simple-plotly-dash-app-to-heroku-622a2216eb73).
-* I also have a gallery of simple Dash apps for learning [here](https://github.com/austinlasseter/plotly_dash_tutorial/blob/master/06%20Heroku%20examples/list%20of%20resources.md).
-* If you'd like to learn even more about Plotly Dash, check out my [tutorial repo](https://github.com/austinlasseter/plotly_dash_tutorial) on github!.
-* If you'd like to tinker with the colors of your app, try using HEX codes from [HTML Color Codes](https://htmlcolorcodes.com/).
-* The `assets` folder contains a file called `favicon.ico` -- you can find and download customized favicons [here](https://www.favicon.cc/). Just replace the current favicon with a new one.
-* Plotly Dash apps can only be viewed in a modern browser (like Chrome or Mozilla). They won't render in antediluvian browsers such as Microsoft.
+The first chart we can make using this player's pitch data is a scatter plot. We'll take each pitch and place it on the x-axis based on the order in which it was thrown. The first pitch in the data set will be all the way to the left and the last pitch in the data set will be all the way to the right.
+
+On the y-axis we will plot the velocity of each pitch. The slowest pitch in the data set will be at the bottom while the fastest pitch in the data set will be at the top. In order to make the different pitches stand out from one another, we'll use different colors to plot each pitch.
+
+The second chart we can make is a box plot. For this chart, we'll group similar pitches together on the x-axis. The y-axis will be the same as the scatter plot- the release speed of the pitch. We'll draw a shape around each pitch type describing the 25th and 75th percentiles as well as the median. Any points past the whiskers are outliers.
--- app.py
+++ app.py
@@ -5,84 +5,117 @@ from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
 import pybaseball as pb
 import plotly.express as px
+import dash_ace
+from datetime import datetime as dt
 
-external_stylesheets = ['https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css']
+external_stylesheets = [
+    "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.title = 'Baseball Pitches Visualizer'
 server = app.server
+
+intro = dcc.Markdown(
+    children="""
+  # Baseball Pitches Visualizer
   
-app.layout = html.Div(className="mt-5 mr-5 ml-5", children=[
-  dcc.Markdown(children="## Using Data Visualization To Understand Baseball Pitching"),
-  html.Br(),
-  html.Br(),
-  dcc.Markdown(children="### The game of baseball places a strong emphasis on statistics. That means that the game's history is accessible for those of us who weren't there to witness it. For example, did you know that [Sandy Koufax is the only pitcher to throw a perfect game while striking out a batter in every inning?](https://en.wikipedia.org/wiki/Sandy_Koufax%27s_perfect_game)"),
-  dcc.Markdown(children="### There are three common types of pitches thrown by modern pitchers: the [fastball](https://en.wikipedia.org/wiki/Fastball), the [breaking ball](https://en.wikipedia.org/wiki/Breaking_ball), and the [change-up](https://en.wikipedia.org/wiki/Changeup). Let's use data visualization to better understand the difference between these pitch types."),
-  dcc.Markdown(children="### In the input boxes below, type the first and last name of a player who threw a pitch in the 2019 baseball season."),
-  html.Br(),
-  dcc.Markdown(children="#### First Name"),
-  dcc.Input(id='first-name', value='Gerrit', type='text'),
-  html.Br(),
-  html.Br(),
-  dcc.Markdown(children="#### Last Name"),
-  dcc.Input(id='last-name', value='Cole', type='text'),
-  html.Br(),
-  html.Br(),
-  dcc.Markdown(children="### We can create a dataset that contains every pitch this player threw in the 2019 baseball season using the incredible `pybaseball` Python API."),
-  html.Div(className="mt-5 mr-5 ml-5", children=[dcc.Markdown(children="`import pybaseball as pb`"),
-  dcc.Markdown(children="`key = pb.playerid_lookup(last_name, first_name)['key_mlbam'].values[0] # get unique pitcher identifier`"),
-  dcc.Markdown(children="`data = pb.statcast_pitcher('2019-03-20', '2019-10-30', key) # get dataset of pitches thrown by pitcher in the 2019 baseball season`"),
-  dcc.Markdown(children="`data = data.sort_values(['pitch_number']) # sort pitches by order thrown, earliest first`"),
-  dcc.Markdown(children="`data = data.dropna(subset=['pitch_type']) # make sure dataset does not contain nulls`"),
-  dcc.Markdown(children="`data['order'] = data.reset_index().index # create new column with pitch order`")]),
-  dcc.Markdown(children="### The first chart we can make using this data is a [scatter plot](https://en.wikipedia.org/wiki/Scatter_plot). To do so, we'll take each pitch and place it on the x-axis based on the order in which it was thrown. The first pitch will be all the way to the left and the last pitch will be all the way to the right."),
-  dcc.Markdown(children="### On the y-axis we will plot the velocity of each pitch. The slowest pitch will be at the bottom while the fastest pitch is at the top. In order to tell the difference between the pitches on the scatter plot, we'll use different colors for each pitch type."),
-  html.Div(className="mt-5 mr-5 ml-5", children=[dcc.Markdown(children="`import plotly.express as px`"),
-  dcc.Markdown(children="`scatter = px.scatter(data, x='order', y='release_speed', color=pitch_type',title=f'{first_name} {last_name} 2019 Pitches Release Speed Scatter Plot')`")]),
-  dcc.Markdown(children="### The second chart we can make is a [box plot](https://en.wikipedia.org/wiki/Box_plot). For this chart, we'll group similar pitches together on the x-axis. The y-axis will be the same as the scatter plot- the release speed of the pitch. We'll draw a shape around each pitch type describing the 25th and 75th percentiles as well as the median. Any points past the whiskers are outliers."),
-  html.Div(className="mt-5 mr-5 ml-5", children=dcc.Markdown(children="`box = px.box(data, x='pitch_type', y='release_speed', color='pitch_type', title=f'{first_name} {last_name} 2019 Pitches Release Speed Box Plot')`")),
-  html.Br(),
-  html.Br(),
-  html.Button('Create Charts', id='button', className="btn btn-primary mb-5"),
-  dcc.Markdown(children="Scatter Plot"),
-  dcc.Loading(children=dcc.Graph(id='scatter')),
-  dcc.Markdown(children="Box Plot"),
-  dcc.Loading(children=dcc.Graph(id='box')),
-  dcc.Markdown(children="### built by [Joe Damiba](https://josephdamiba.com)"),
-])
+  In the input boxes below, type the first and last name of a Major League Baseball pitcher.
+  
+  Also, enter a date range. Then, click create charts!
+  
+  #### First Name
+  """
+)
+
+app.layout = html.Div(
+    className="mt-1 mr-5 ml-5",
+    children=[
+        intro,
+        dcc.Input(className="mb-3", id="first-name", value="Gerrit", type="text"),
+        dcc.Markdown(children="#### Last Name"),
+        dcc.Input(id="last-name", value="Cole", type="text"),
+        html.Br(),
+        html.Br(),
+        dcc.Markdown(children="#### Date Range"),
+        dcc.DatePickerRange(
+            id="date-picker",
+            min_date_allowed=dt(2019, 3, 20),
+            max_date_allowed=dt(2019, 9, 29),
+            initial_visible_month=dt(2019, 3, 20),
+        ),
+        html.Br(),
+        html.Button(
+            "Create Charts!", id="button", className="btn btn-primary mt-3 mb-3"
+        ),
+        dcc.Loading(children=dcc.Graph(id="scatter")),
+        dcc.Loading(children=dcc.Graph(id="box")),
+    ],
+)
+
 
 @app.callback(
-    [Output(component_id='scatter', component_property='figure'),
-     Output(component_id='box', component_property='figure')],
-    [Input('button', 'n_clicks')],
-    [State(component_id='first-name', component_property='value'),
-     State(component_id='last-name', component_property='value')])
-def update_output_div(n_clicks, first_name, last_name):
-  #only update on increment
-  prev_clicks = 0
-  if n_clicks is None or n_clicks == prev_clicks:
-    raise PreventUpdate
-  else:     
-    try:
-      key = pb.playerid_lookup(last_name, first_name)["key_mlbam"].values[0] # get unique pitcher identifier
-    except:
-      return px.scatter(title="No player found with that name. Check spelling?"), px.box(title="No player found with that name. Check spelling?"), 
-    
-    data = pb.statcast_pitcher("2019-03-20", "2019-10-30", key) # get dataset of pitches thrown by pitcher
-    data = data.sort_values(["pitch_number"]) # sort pitches by order thrown, earliest first
-    data = data.dropna(subset=['pitch_type']) # make sure dataset does not contain nulls
-    
-    data["order"] = data.reset_index().index # create new column with pitch order
-        
-    scatter = px.scatter(data, x="order", y="release_speed", color="pitch_type", 
-                         title=f'{first_name} {last_name} 2019 Pitches Release Speed Scatter Plot')
-    
-    box = px.box(data, x="pitch_type", y="release_speed", color="pitch_type", 
-                title=f'{first_name} {last_name} 2019 Pitches Release Speed Box Plot')
-    
-    prev_clicks = prev_clicks + 1
-    
-    return scatter, box
-  
-if __name__ == '__main__':
+    [
+        Output(component_id="scatter", component_property="figure"),
+        Output(component_id="box", component_property="figure"),
+    ],
+    [
+        Input("button", "n_clicks"),
+        Input("date-picker", "start_date"),
+        Input("date-picker", "end_date"),
+    ],
+    [
+        State(component_id="first-name", component_property="value"),
+        State(component_id="last-name", component_property="value"),
+    ],
+)
+def update_output_div(n_clicks, start_date, end_date, first_name, last_name):
+    # only update on increment
+    prev_clicks = 0
+    if n_clicks is None or n_clicks == prev_clicks:
+        raise PreventUpdate
+    elif start_date is None or end_date is None:
+        raise Exception("Date cannot be empty")
+    else:
+        try:
+            key = pb.playerid_lookup(last_name, first_name)["key_mlbam"].values[
+                0
+            ]  # get unique pitcher identifier
+        except:
+            pass
+
+        data = pb.statcast_pitcher(
+            start_date, end_date, key
+        )  # get dataset of pitches thrown by pitcher
+        data = data.sort_values(
+            ["pitch_number"]
+        )  # sort pitches by order thrown, earliest first
+        data = data.dropna(
+            subset=["pitch_type"]
+        )  # make sure dataset does not contain nulls
+
+        data["order"] = data.reset_index().index  # create new column with pitch order
+
+        scatter = px.scatter(
+            data,
+            x="order",
+            y="release_speed",
+            color="pitch_type",
+            title=f"Scatter Plot of {first_name} {last_name}'s Pitch Release Speed For Between {start_date} and {end_date}",
+        )
+
+        box = px.box(
+            data,
+            x="pitch_type",
+            y="release_speed",
+            color="pitch_type",
+            title=f"Box Plot of {first_name} {last_name}'s Pitch Release Speed For Between {start_date} and {end_date}",
+        )
+
+        prev_clicks = prev_clicks + 1
+
+        return scatter, box
+
+
+if __name__ == "__main__":
     app.run_server(debug=True)
-    
